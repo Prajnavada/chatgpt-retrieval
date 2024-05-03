bd60a69c9751fa13fa69d47a80b5f5eb7ed45d13
Kaushal <kaushal.beyblader@gmail.com>
2024-05-02 23:42:58 -0700

    Update __init__.py

--- prompt/__init__.py
+++ prompt/__init__.py
@@ -1,1 +1,2 @@
-from .prompt import qa_template
+from .prompt import qa_prompt, react_prompt, react_chat_prompt, react_chat_json_prompt, react_multi_input_prompt, structured_chat_prompt
+from .self import self_discovery_structure, self_discovery_reasoning, self_discovery_select, self_discovery_adapt
